c472212f7d098f11697efb87e00f1ed20c072ab4
Dhruba Jyoti Bharali <official.dhruvb@gmail.com>
2023-09-17 19:01:07 +0530

    Formated time, and button functionality

--- script.js
+++ script.js
@@ -9,55 +9,79 @@ const btnStart = document.getElementById('btnStart');
 const btnReStart = document.getElementById('btnReStart');
 const btnPause = document.getElementById('btnPause');
 
-let initial = '00:00:00';
+let initial = '00:25:00';
 countDown.innerText = initial;
-let min = 1;
+let min = 25;
 let hour = 0;
-let sec = 0;
-
+let sec = 5;
+var time;
 
 
 btnStart.addEventListener('click', () => {
+    clearInterval(time);
     console.log('Btn Start Clicked')
+    min = 1;
+    sec = 0;
     startTheClock();
 })
 
 btnReStart.addEventListener('click', () => {
+    initial = '00:25:00';
+    countDown.innerText = initial;
+    console.log('Btn Re-Start Clicked')
+    clearInterval(time);
+    min = 1;
+    sec = 0;
     startTheClock();
 })
 
+btnPause.addEventListener('click', () => {
+    clearInterval(time)
+    if(btnPause.innerText == 'RESUME'){
+        startTheClock()
+    }
+    var currentTime = countDown.innerText;
+    var timeArray = currentTime.split(':');
+    min = Number(timeArray[1]);
+    sec = Number(timeArray[2]);
+    btnPause.innerHTML = btnPause.innerText == 'PAUSE' ? 'RESUME' : 'PAUSE';
+    console.log(timeArray);
+})
 
 function startTheClock() {    
-    console.log('Enter into startTheClock, with Initial value: ' +  initial)
-    var time = setInterval(() => {
-        console.log('Entering into SetInterval'+ (min ==0))
-        if(min == 0 && sec == 0){
-            //send reminder 
-            min = 5;
-            sec = 0;
-            // countDown.innerText = initial;
+    time = setInterval(() => {
+        if(hour == 0 && min == 0 && sec == 0){
             clearInterval(time);
             userInput();
         }
-
-        initial = `${hour}:${min}:${sec}`
-        if(sec == 0 && min != 0){
+        if(hour != 0 && min == 0 && sec == 0){
+            hour = hour - 1
+            min = 60
+        }
+        if(min != 0 && sec == 0){
             min = min - 1;
-            sec = 59;
-            console.log('inside first if with hour: '+hour+ ' and min: '+ min)
+            sec = 60
+            initial = Display();
+            countDown.innerText = initial;
         }
-
         if(sec != 0){
-            sec = sec - 1
+            sec = sec - 1;
+            initial = Display();
+            countDown.innerText = initial;
         }
-
-        
-
-        countDown.innerText = initial;
-    }, 1000)
-    
+    }, 1000);
 }
 
 function userInput() {
+    confirm("Hey Buddy It's time for a break!!!")
+    if(confirm){
+        min = 5;
+        sec = 0;
+        startTheClock();
+    }
+}
 
+function Display() {
+    
+    return formattedTime = `${String(hour).padStart(2, '0')}:${String(min).padStart(2, '0')}:${String(sec).padStart(2, '0')}`;
 }
